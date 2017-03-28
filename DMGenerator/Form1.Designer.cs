56121367ba99eac61717ea0d76414ee02fd40567
470642298@qq.com“你的github用户名 <470642298@qq.com>
2017-03-28 10:38:27 +0800

    Data_matrix_gen
    developed by vs 2015

---
+++ DMGenerator/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace DMGenerator
+{
+    partial class DataMatrixGenerator
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputFileEdit = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.browseInputBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.outputDirEdit = new System.Windows.Forms.TextBox();
+            this.browseOutputBtn = new System.Windows.Forms.Button();
+            this.matrixSizeCB = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pointSizeCB = new System.Windows.Forms.ComboBox();
+            this.genBtn = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // inputFileEdit
+            // 
+            this.inputFileEdit.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputFileEdit.Location = new System.Drawing.Point(71, 12);
+            this.inputFileEdit.Name = "inputFileEdit";
+            this.inputFileEdit.Size = new System.Drawing.Size(331, 21);
+            this.inputFileEdit.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "输入文件";
+            // 
+            // browseInputBtn
+            // 
+            this.browseInputBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.browseInputBtn.Location = new System.Drawing.Point(408, 10);
+            this.browseInputBtn.Name = "browseInputBtn";
+            this.browseInputBtn.Size = new System.Drawing.Size(75, 23);
+            this.browseInputBtn.TabIndex = 2;
+            this.browseInputBtn.Text = "浏览";
+            this.browseInputBtn.UseVisualStyleBackColor = true;
+            this.browseInputBtn.Click += new System.EventHandler(this.browseInputBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "输出目录";
+            // 
+            // outputDirEdit
+            // 
+            this.outputDirEdit.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputDirEdit.Location = new System.Drawing.Point(71, 52);
+            this.outputDirEdit.Name = "outputDirEdit";
+            this.outputDirEdit.Size = new System.Drawing.Size(331, 21);
+            this.outputDirEdit.TabIndex = 4;
+            // 
+            // browseOutputBtn
+            // 
+            this.browseOutputBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.browseOutputBtn.Location = new System.Drawing.Point(408, 50);
+            this.browseOutputBtn.Name = "browseOutputBtn";
+            this.browseOutputBtn.Size = new System.Drawing.Size(75, 23);
+            this.browseOutputBtn.TabIndex = 5;
+            this.browseOutputBtn.Text = "浏览";
+            this.browseOutputBtn.UseVisualStyleBackColor = true;
+            this.browseOutputBtn.Click += new System.EventHandler(this.browseOutputBtn_Click);
+            // 
+            // matrixSizeCB
+            // 
+            this.matrixSizeCB.FormattingEnabled = true;
+            this.matrixSizeCB.Items.AddRange(new object[] {
+            "0",
+            "16",
+            "18",
+            "20",
+            "22",
+            "24",
+            "26",
+            "32"});
+            this.matrixSizeCB.Location = new System.Drawing.Point(119, 92);
+            this.matrixSizeCB.Name = "matrixSizeCB";
+            this.matrixSizeCB.Size = new System.Drawing.Size(121, 20);
+            this.matrixSizeCB.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "尺寸（0=自动）";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "半径";
+            // 
+            // pointSizeCB
+            // 
+            this.pointSizeCB.FormattingEnabled = true;
+            this.pointSizeCB.Items.AddRange(new object[] {
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15"});
+            this.pointSizeCB.Location = new System.Drawing.Point(119, 131);
+            this.pointSizeCB.Name = "pointSizeCB";
+            this.pointSizeCB.Size = new System.Drawing.Size(121, 20);
+            this.pointSizeCB.TabIndex = 9;
+            // 
+            // genBtn
+            // 
+            this.genBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.genBtn.Location = new System.Drawing.Point(336, 88);
+            this.genBtn.Name = "genBtn";
+            this.genBtn.Size = new System.Drawing.Size(145, 63);
+            this.genBtn.TabIndex = 10;
+            this.genBtn.Text = "生成";
+            this.genBtn.UseVisualStyleBackColor = true;
+            this.genBtn.Click += new System.EventHandler(this.genBtn_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(13, 166);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(468, 23);
+            this.progressBar1.TabIndex = 11;
+            // 
+            // DataMatrixGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 201);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.genBtn);
+            this.Controls.Add(this.pointSizeCB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.matrixSizeCB);
+            this.Controls.Add(this.browseOutputBtn);
+            this.Controls.Add(this.outputDirEdit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.browseInputBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputFileEdit);
+            this.Name = "DataMatrixGenerator";
+            this.Text = "DataMatrix生成器";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputFileEdit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button browseInputBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox outputDirEdit;
+        private System.Windows.Forms.Button browseOutputBtn;
+        private System.Windows.Forms.ComboBox matrixSizeCB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox pointSizeCB;
+        private System.Windows.Forms.Button genBtn;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ DMGenerator/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+using DataMatrix.net;
+
+namespace DMGenerator
+{
+    public partial class DataMatrixGenerator : Form
+    {
+        EventWaitHandle exitEvent;
+
+        bool working;
+
+        public DataMatrixGenerator()
+        {
+            InitializeComponent();
+            matrixSizeCB.SelectedIndex = 0;
+            pointSizeCB.SelectedIndex = 0;
+            working = false;
+        }
+
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            exitEvent.Set();
+        }
+
+        private void genBtn_Click(object sender, EventArgs e)
+        {
+            if (working)
+            {
+                cancelBtn_Click(sender, e);
+                return;
+            }
+
+            //检查输入框
+            if (!File.Exists(inputFileEdit.Text))
+            {
+                MessageBox.Show("输入文件错误", "错误");
+                return;
+            }
+
+            if (!Directory.Exists(outputDirEdit.Text))
+            {
+                MessageBox.Show("目标目录错误", "错误");
+                return;
+            }
+
+            DmtxImageEncoderOptions opt = new DmtxImageEncoderOptions();
+            switch(int.Parse(matrixSizeCB.Text))
+            {
+                case 0: opt.SizeIdx = DmtxSymbolSize.DmtxSymbolSquareAuto; break;
+                case 16: opt.SizeIdx = DmtxSymbolSize.DmtxSymbol16x16; break;
+                case 18: opt.SizeIdx = DmtxSymbolSize.DmtxSymbol18x18; break;
+                case 20: opt.SizeIdx = DmtxSymbolSize.DmtxSymbol20x20; break;
+                case 22: opt.SizeIdx = DmtxSymbolSize.DmtxSymbol22x22; break;
+                case 24: opt.SizeIdx = DmtxSymbolSize.DmtxSymbol24x24; break;
+                case 26: opt.SizeIdx = DmtxSymbolSize.DmtxSymbol26x26; break;
+                case 32: opt.SizeIdx = DmtxSymbolSize.DmtxSymbol16x16; break;
+                default: MessageBox.Show("程序可能被改坏了", "错误"); return; break;
+            }
+
+            int pointSize = int.Parse(pointSizeCB.Text) * 2;
+            string outputDir = outputDirEdit.Text;
+
+            string[] inputDatas = null;
+
+            try
+            {
+                inputDatas = File.ReadAllLines(inputFileEdit.Text);
+            }
+            catch
+            {
+                MessageBox.Show("读取输入文件错误", "错误");
+                return;
+            }
+
+            progressBar1.Maximum = inputDatas.Length;
+            progressBar1.Value = 0;
+
+            exitEvent = new EventWaitHandle(false, EventResetMode.ManualReset);
+
+            Action work = new Action(() =>
+            {
+                Action updateProgAction = new Action(() => progressBar1.Value = progressBar1.Value + 1);
+                DmtxImageEncoder encoder = new DmtxImageEncoder();
+
+                int dataLen = inputDatas.Length;
+                for(int index = 0; index < dataLen; ++index)
+                {
+                    string text = inputDatas[index];
+                    if (exitEvent.WaitOne(0))
+                        break;
+
+                    Invoke(updateProgAction);
+
+                    string destFile = Path.Combine(outputDir,  (index + 1).ToString()) + ".png";
+                    File.Delete(destFile);
+
+                    bool[,] r = encoder.EncodeRawData(text, opt);
+
+                    if (r == null)
+                    {
+                        object shouldCont = false;
+                        Invoke(new Action(()=> {
+                            if (MessageBox.Show("所选尺寸不足，无法处理第" + (index + 1).ToString() + "行数据。是否继续？", "错误", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                                shouldCont = true;
+                        }));
+
+                        if (((bool)shouldCont) == false)
+                            break;
+                        else
+                            continue;
+                    }
+
+                    int wc = r.GetLength(0);
+                    int hc = r.GetLength(1);
+
+                    int padding = 0;
+                    pointSize += padding;
+
+                    Bitmap bmp = new Bitmap(850,850);
+                    Graphics g = Graphics.FromImage(bmp);
+                    g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+                    g.FillRectangle(Brushes.White, Rectangle.FromLTRB(0, 0, 850,850));
+                    
+                    for(int j = 0; j < hc; ++j)
+                        for(int i = 0; i < wc; ++i)
+                            if (r[i, j])
+                            {
+                                Rectangle rect = Rectangle.FromLTRB(i * 45 + padding, j * 45 + padding, (i + 1) * 45 - 1, (j + 1) * 45 - 1);
+                                g.DrawEllipse(Pens.Black, rect);
+                                g.FillEllipse(Brushes.Black, rect);
+                                //g.DrawRectangle(Pens.Black, rect);
+                                //g.FillRectangle(Brushes.Black, rect);
+                            }
+                    g.Flush();
+                    g.Dispose();
+
+                    bmp.Save(destFile, System.Drawing.Imaging.ImageFormat.Png);
+                }
+
+                Invoke(new Action(() => { working = false; genBtn.Text = "生成"; }));
+            });
+
+            ThreadStart ts = new ThreadStart(work);
+            Thread workThread = new Thread(ts);
+            workThread.SetApartmentState(ApartmentState.STA);
+            working = true;
+            genBtn.Text = "取消";
+            workThread.Start();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (working)
+            {
+                MessageBox.Show("生成过程中无法退出，请先停止", "错误");
+                e.Cancel = true;
+            }
+        }
+
+        private void browseInputBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "文本文件|*.txt";
+            ofd.RestoreDirectory = true;
+            try { ofd.InitialDirectory = Path.GetDirectoryName(inputFileEdit.Text); } catch { }
+            if (ofd.ShowDialog(this) == DialogResult.OK)
+            {
+                inputFileEdit.Text = ofd.FileName;
+            }
+        }
+
+        private void browseOutputBtn_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            try { fbd.SelectedPath = outputDirEdit.Text; } catch { }
+            if (fbd.ShowDialog(this) == DialogResult.OK)
+            {
+                outputDirEdit.Text = fbd.SelectedPath;
+            }
+        }
+    }
+}
